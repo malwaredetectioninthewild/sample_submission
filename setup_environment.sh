80f16b007a46b123c5ad1320710847947a559fb0
malwaredetectioninthewild <140429732+malwaredetectioninthewild@users.noreply.github.com>
2023-12-06 16:49:03 -0500

    fixed the environment setup script

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,4 +1,4 @@
-conda create -y sample_submission python=3.8 pip
+conda create --name sample_submission -y python=3.8 pip
 conda activate sample_submission
 
 conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
